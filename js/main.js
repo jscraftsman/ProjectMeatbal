8e459dfc4dae51298378f97f53b473bcf5640846
Gervel Giva <gervelgiva@gmail.com>
2013-01-11 16:21:17 +0800

    Login and signup: incomplete

--- js/main.js
+++ js/main.js
@@ -3,12 +3,10 @@ var DEFAULT = {};
 
 $(function() {
 	init();
-	$(window).resize(function() {
-		setWindowDimension();
-		setSize($("body"), DEFAULT.W, DEFAULT.H);
-	});
+	$(window).resize(function() { setWindowDimension(); setSize($("body"), DEFAULT.W, DEFAULT.H); });
+	$("#login-signup button").click(function(){	processAccountAction(this);	});
 	
-	setTimeout(function(){$("#loading").hide();}, 1000);
+	setTimeout(function(){$("#loading").hide();}, 500);
 });
 
 function hasLocalStorage() {
@@ -18,9 +16,45 @@ function hasLocalStorage() {
 }
 function setSize(dom, w, h){ dom.css({ "width" : w, "height" : h });	}
 function setWindowDimension(){ DEFAULT.W = window.innerWidth; DEFAULT.H = window.innerHeight;	}
+function toggleForm(form){ $("#login-signup #" + form).toggle(); 	}
 
+/* incomplete! */
+function processAccountAction(button){
+	var parentID = button.parentNode.parentNode.id;
+	var buttonID = button.id;
+	var msg = $("#" + parentID).find("#msg");
+	var user = $("#" + parentID).find("#username").val();
+	var pass = $("#" + parentID).find("#password").val();
+	if(parentID == "login"){
+		if(buttonID == "loginbtn"){
+			if(validateLogin(user, pass)){
+				msg.text("Login success!");
+				$("#login-signup").hide();
+			}else{
+				msg.text("Incorrect username or password!");
+			}
+		}else{
+			toggleForm("login");
+			toggleForm("signup");
+		}
+	}else{
+		if(buttonID == "signupbtn"){
+			alert("signup done");
+			toggleForm("login");
+			toggleForm("signup");
+		}else{
+			toggleForm("login");
+			toggleForm("signup");
+		}
+	}
+}
+
+function validateLogin(user, pass){
+	var dummyuser = "user", dummypass = "1234qwer";
+	return(user == dummyuser && pass == dummypass);
+}
 function init(){
 	setWindowDimension();
 	setSize($("body"), DEFAULT.W, DEFAULT.H);
-	
+	toggleForm("signup");
 }
